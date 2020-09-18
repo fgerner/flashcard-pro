c607eecfe384e974007796e1660bb477bce0c3c4
Fred <fred.gerner@icloud.com>
2020-09-18 21:24:21 +1000

    added stack route

--- src/components/StackList.js
+++ src/components/StackList.js
@@ -1,14 +1,17 @@
 import React, {Component} from 'react';
 import stacks from '../data/stacks.json';
+import {Link} from 'react-router-dom';
 
 class StackList extends Component {
     render() {
         return (
             <div>
                 {stacks.map(stack => {
-                  return(
-                      <h4>{stack.title}</h4>
-                  )
+                    return (
+                        <Link to='/stack'>
+                            <h4 key={stack.id}>{stack.title}</h4>
+                        </Link>
+                    )
                 })
                 }
             </div>
